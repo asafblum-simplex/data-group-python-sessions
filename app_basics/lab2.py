5363da9758f27dccb2a5ad2b130ea1f0f2aaca7f
Asaf Blum <asaf.blum@simplex.com>
2022-08-17 16:21:10 +0300

    lab2.py

---
+++ app_basics/lab2.py
@@ -0,0 +1,60 @@
+"""
+NO GOOGLING ALLOWED FOR THIS LAB
+You can consult with each other :)
+
+Try to write the function definitions first; consider the input(parameters and types of each param) and return value
+before writing any other code.
+
+For each solution, prefer the most generic implementation
+"""
+
+"""
+1. 
+
+As a user, i want to input a start number and an end number.
+The program should print each number with the following exceptions:
+ - For multiples of three print "Fizz" instead of the number.
+ - for the multiples of five print "Buzz".
+ - For numbers which are multiples of both three and five print "FizzBuzz".
+
+Otherwise, print the number
+"""
+
+"""
+2. 
+
+As a user, i want to be able to get the nth item in a fibonacci sequence by inputting the number i want, and the pair 
+of numbers from which to start.
+The output should also include the sum-total of all values in the sequence.
+
+Example usage: 
+fibo(10, 0, 1) # means, give me the 10th member of the fibo seq, starting from 0 and 1 
+fibo(50, 13, 21) # means, give me the 50th member of the fibo seq, starting from 13 and 21  
+"""
+
+"""
+3. (thinking exercise)
+
+After completing the above, think. What did you learn from the implementations? (could be nothing)
+
+Try to articulate it into a few sentences.
+"""
+
+"""
+4. (research exercise)
+
+FOR THIS QUESTION, GOOGLE (and any other information source) IS ALLOWED
+
+Think about which questions you still have (regarding what we covered thus far, either in sessions or by self-learning).
+(If you have questions about more advancded topics than what we have covered, just put them aside for now and we'll get 
+back to it soon)
+Write down 5 questions that you dont know the answer to (yet).
+Try to answer each question on your own the best way that you can.
+Write down the explanation in such way that will also be clear to other people.
+
+Repeat the process until you have 5 questions that you are unable to answer on your own.
+Partial answers also applies. Which means you will still need to find another question to add to the list.
+
+Send me your list (either privately or in this group).
+We will discuss the list you prepared together.
+"""
