7050a17cce8783277f9e8b525a9e650d1afaa852
Asaf Blum <asaf.blum@simplex.com>
2022-08-17 16:36:16 +0300

    lab1.py updated instructions

--- app_basics/lab1.py
+++ app_basics/lab1.py
@@ -2,21 +2,16 @@
 Create a cli tool that will keep running, waiting for user input, evaluating and printing.
 
 As a user, I want to be able to input a file path(hint, see os.path), apply head/tail on the file content, and see the output  (for example first 3 lines)
-As a user, I want to be able to input a csv line, apply head/tail on the line content, and see the output(for example first 3 columns)
+As a user, I want to be able to input a csv line,  apply head/tail on the line content, and see the output(for example first 3 columns)
 As a user, I want to be able to be able to exit with a special command (for example '//exit')
-
 As a user, I want to be able to input anything else and see only the head/tail
 As a user, I want the program to output the accumulated first word of every line
 
 The tool should print(meaningfully) the exact date and time in which it started, and in which it ended.
 
-
-save current user input, while input is not exact match '//exit', keep running... (while loop with condition)
-
-testing conditions for each line:
-
-1. test if 'exit' cmd, if true -> somehow to exit the while loop -
+P.S Think what happens in the future, when product wants us to add a new command
 """
+
 from commands.validators import is_exit_cmd
 
 if __name__ == '__main__':
